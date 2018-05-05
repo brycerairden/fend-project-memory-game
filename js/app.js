16621e38ee27f0299655b17684758a9157c407e4
brairden <26549104+brairden@users.noreply.github.com>
2018-05-05 03:36:09 -0600

    style: Add comments and edits based on Udacity style guides

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-/*
- * Create a list that holds all of your cards
- */
+/* Creates a list that holds all of cards */
 
 let card = document.querySelectorAll(".card");
 let cards = [...card];
@@ -8,10 +6,10 @@ let cards = [...card];
 const deck = document.querySelector(".deck");
 
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
+ * Displays the cards on the page
+ *   - shuffles the list of cards
+ *   - loops through each card and create its HTML
+ *   - adds each card's HTML to the page
  */
 
 function deal() {
@@ -19,7 +17,7 @@ function deal() {
 	cardDeck.forEach(function(c) {
 		deck.appendChild(c);
 	})
-};
+}
 
 window.onload = deal();
 
@@ -50,26 +48,29 @@ function shuffle(array) {
  *    + score() if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
-
+// Adds "open" class to the card the user clicked on
 function displayCard(clickedCard) {
 	clickedCard.classList.add('open');
-};
+}
 
+// Removes the "open" class to hid the card when it is unmatched or the game is reset after a .8 second delay
 function hideCard(card) {
 	setTimeout(function () {card.classList.remove('open')}, 800);
-};
+}
 
+// Array that will hold the 2 open cards for comparision reasons
 let openCards = [];
 
+// Adds the user clicked card to an array that can be used to compare cards
 function openCardList(clickedCard) {
 	openCards.push(clickedCard);
-	console.log(openCards);
-};
+}
 
+// Variable to count matches, so I know when all cards have been matched
 let matches = 0;
 
+// If 2 cards are a match, it removes the "open" class and replaces it with the "match" class while incrementing the number of matches overall
 function matched() {
-	console.log ("matched");
 	openCards.forEach(function(card) {
 		hideCard(card)
 	});
@@ -77,36 +78,36 @@ function matched() {
 		card.classList.add("match");
 	});
 	matches++;
-};
+}
 
+// if 2 cards are not a match, return them to their original state to be reused
 function unmatched() {
-	console.log ("unmatched")
 	openCards.forEach(function(card) {
 		hideCard(card)
 	});
-};
+}
 
+// Move counter and display of count
 const moveCount = document.querySelectorAll(".moves")[0]
+
 function count() {
 	counter++;
 	moveCount.innerHTML = counter;
-};
+}
 
+// Stops the game with the win condition when 8 matches (16 cards) are matched
 function score() {
 	if (matches === 8) {
-		console.log("You WIN!")
 		stopTimer();
-		console.log(`Your time was ${endMinutes} min ${endSeconds} sec.`)
 		modal.style.display = "block";
 		document.querySelectorAll(".modal-moves")[0].innerHTML = counter;
 		document.querySelectorAll(".modal-time")[0].innerHTML = `Your time was ${endMinutes} min ${endSeconds} sec.`;
 		const starsEarned = document.querySelectorAll(".stars")[0].innerHTML;
 		document.querySelectorAll(".modal-stars")[0].innerHTML = starsEarned;
-		// congradulations modal, with replay ask and link to restart function, time to win, and star rating
 	}
-};
+}
 
-// restart function, suffles and resets all values associated with the timer, move counter, star rating, and matches made
+// Restart function, suffles and resets all values associated with the timer, move counter, star rating, and matches made
 const restart = document.querySelectorAll(".restart")[0];
 const modalRestart = document.querySelectorAll(".modal-restart")[0]
 
@@ -128,7 +129,7 @@ function restartGame() {
 	timerStarted = false;
 	matches = 0;
 	modal.style.display = "none";
-};
+}
 
 restart.addEventListener("click", function() {
 	restartGame();
@@ -138,7 +139,7 @@ modalRestart.addEventListener("click", function() {
 	restartGame();
 });
 
-//timer function
+// Timer function
 let seconds = 0,
 minutes = 0,
 activeTimer,
@@ -169,6 +170,8 @@ function stopTimer() {
 	clearInterval(activeTimer);
 }
 
+
+// Star Rating function
 let counter = 0;
 const displayStars = document.querySelectorAll(".stars")[0];
 
@@ -178,19 +181,20 @@ function stars() {
 	} else if (counter === 25) {
 		displayStars.children[1].style.visibility = "hidden";
 	}
-};
+}
 
+// Event listener set to the ul class, passing along the info on the LI target
 deck.addEventListener("click", function(event) {
 	let clickedCard;
 	if (event.target.nodeName === "UL") {
 		return false;
-	} else	if (event.target.nodeName === "LI") {
+	} else if (event.target.nodeName === "LI") {
 		clickedCard = event.target;
-	} else if (event.target.nodeName === "I") {
-		clickedCard = event.target.parentElement;
 	} else if (event.target.nodeName === "IMG") {
 		clickedCard = event.target.parentElement;
 	};
+
+	// Below ignores matched, or open, before checking if the timer is on, turning it on if not, compares the cards
 	if ((clickedCard.classList.contains("match") === false) && (clickedCard.classList.contains("open") === false)) {
 		if (timerStarted === false) {
 			timerStarted = true;
@@ -208,6 +212,7 @@ deck.addEventListener("click", function(event) {
 			count();
 		};
 	};
+
 	stars();
 	score();
 });
@@ -223,11 +228,11 @@ const closeModal = document.querySelectorAll(".close-modal")[0];
 // When the user clicks on <span> (x), close the modal
 closeModal.onclick = function() {
     modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
+};
